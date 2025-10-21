28e3b7599b3b0ba79f4e385eb6a9f109c09b1758
Mayank Kumar <mayankkumar200311@gmail.com>
2025-10-21 13:36:16 +0530

    Second commit

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Hero from '../Component/Hero'
 import Featured from '../Component/Featured'
 import About from '../Component/About'
-import Images from '../Component/images'
+import Images from '../Component/Images'
 import OurRooms from '../Component/OurRooms'
 import OurStores from '../Component/OurStores'
 import Footer from '../Component/Footer';
